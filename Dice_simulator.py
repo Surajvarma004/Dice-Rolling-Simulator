cd4f9a3fe58add7293013902501b5358e07367a6
Suraj varma <109133892+Surajvarma004@users.noreply.github.com>
2022-09-14 13:03:59 +0530

    Add files via upload

---
+++ Dice_simulator.py
@@ -0,0 +1,76 @@
+import random 
+import time
+from tkinter import Y
+
+
+">>>>> ! DICE SIMULATOR ! <<<<<"
+
+# the person will spin the dice 
+# random number will appear from 1 - 6
+ 
+
+
+ 
+
+while True :
+
+
+ dice_number  = random.randint(1 , 6)
+
+ time.sleep(2)
+
+ if dice_number == 1 :
+        print("[-------]")
+        print("[       ]")
+        print("[   0   ]")
+        print("[       ]")
+        print("[-------]")
+
+
+ if dice_number == 2 :
+        print("[-------]")
+        print("[0      ]")
+        print("[       ]")
+        print("[      0]")
+        print("[-------]")
+
+ if dice_number == 3 :
+        print("[-------]")
+        print("[0     0]")
+        print("[       ]")
+        print("[   0   ]")
+        print("[-------]")
+
+ if dice_number == 4 :
+        print("[-------]")
+        print("[0     0]")
+        print("[       ]")
+        print("[0     0]")
+        print("[-------]")
+
+ if dice_number == 5 :
+        print("[-------]")
+        print("[0     0]")
+        print("[   0   ]")
+        print("[0     0]")
+        print("[-------]")
+ if dice_number == 6 :
+        print("[-------]")
+        print("[0  0  0]")
+        print("[       ]")
+        print("[0  0  0]")
+        print("[-------]")
+
+ a = input("Press yes to continue: ")
+ continue
+ 
+    
+
+
+
+
+
+
+
+
+    
